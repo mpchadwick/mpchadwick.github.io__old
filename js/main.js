7820f80ee9bae5df5c13e884214e0929139c851b
Max Chadwick <mpchadwick@gmail.com>
2013-10-24 19:11:20 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+	/**
+	 * Resize text in "Max Chadwick Object" based on the window width
+	 */
+	var windowWidth, fontSize;
+	
+	function fitMaxChadwickObject() {
+		windowWidth = window.innerWidth;
+		if(windowWidth < 670) {
+			fontSize = windowWidth / 30;
+		} else if(windowWidth >= 670 && windowWidth < 800) {
+			fontSize = '23px';
+		} else if(windowWidth >= 800 && windowWidth < 1000) {
+			fontSize = windowWidth / 50;
+		} else {
+			fontSize = '20px';
+		}
+		$('#max-chadwick-object').css('font-size', fontSize);
+	}
+
+	fitMaxChadwickObject();
+	
+	$('#max-chadwick-object').css('visibility', 'visible'); 
+
+	$(window).resize(function() {
+		fitMaxChadwickObject();
+	})
+
+
+	/** 
+	 * Responsive Nav Functionality 
+	 */
+	$('#nav-toggle').click(function() {
+		$('.nav-link').toggleClass('display-none-link');
+		if($('.nav-link').hasClass('display-none-link')) {
+			$(this).css('margin-bottom', '0px');
+		} else {
+			$(this).css('margin-bottom', '1em');
+		}
+	})
+
+})
